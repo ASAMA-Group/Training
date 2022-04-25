88038118bc2dfd8b9b922257b7acf182b3ad0971
matinsajadi <emtinsajadi79@gmail.com>
2022-04-25 17:56:45 +0430

    add a file nosetests

---
+++ test_in_python/nose/tests/test_operetions.py
@@ -0,0 +1,12 @@
+import operations
+
+
+def test_add():
+    assert operations.add(3,4) == 7
+
+# def test_subtract(a,b):
+#     assert operations.subtract(4,5) == -1
+
+# def test_division(a,b):
+#     assert operations.division(12,6) == 2
+
